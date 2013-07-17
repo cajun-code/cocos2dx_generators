645537fc10c12f98df01bc55b026856d43977f41
Allan Davis <cajun.code@gmail.com>
2013-07-17 15:54:08 -0400

    adding name to printed message

--- lib/cocos2dx/android.rb
+++ lib/cocos2dx/android.rb
@@ -3,7 +3,7 @@ module Cocos2dx
   class Android < Thor
     desc "project <name>", "Create Andorid project for Cocos2Dx"
     def project(name)
-      puts "Creating project"
+      puts "Creating project #{name}"
     end
   end
 end
