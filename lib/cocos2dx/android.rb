c82ff97e29c0623c88d1f62ad65a29b9adb470cf
Allan Davis <cajun.code@gmail.com>
2013-07-17 18:17:25 -0400

    Fixing documentation

--- lib/cocos2dx/android.rb
+++ lib/cocos2dx/android.rb
@@ -1,7 +1,7 @@
 require 'thor'
 module Cocos2dx
   class Android < Thor
-    desc "project <name>", "Create Andorid project for Cocos2Dx"
+    desc "project NAME", "Create Andorid project for Cocos2Dx"
     def project(name)
       puts "Creating project #{name}"
     end
