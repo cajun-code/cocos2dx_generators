95573b4ec9afe71393ac7ab37aef1a7b6a75608b
Allan Davis <cajun.code@gmail.com>
2013-07-18 16:49:34 -0400

    Defining the prompts to gather for the setup of the android nature

--- lib/cocos2dx/android.rb
+++ lib/cocos2dx/android.rb
@@ -11,8 +11,22 @@ module Cocos2dx
       puts "Creating project #{name}"
       set_name name
       create_base_project
+      add_android_nature
     end
     
+    private 
+    
+    def add_android_nature
+      @android_sdk_home = ask("Where is the Android SDK installed?")
+      @android_ndk_home = ask("where is the Android NDK installed?")
+      @cocos2dx_home = ask("where is the Cocos2d-x Library installed?")
+      
+      @bundle_id = ask("What is the Package name for the app i.e. com.example.app?")
+      @app_name = ask("What is the Application Name?")
+      @api_version = ask("What version of the Android do you wish to target?")
+      
+      
+    end
     
   end
 end
